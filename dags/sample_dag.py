d2ceb03c298c20d91936bbd31f1f029328835e14
shivamg-git <gupta.shivam.work@gmail.com>
2024-02-17 00:32:31 +0530

    Updated Dag

--- dags/sample_dag.py
+++ dags/sample_dag.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 from airflow.models import DAG
-from airflow.operators.dummy import DummyOperator
+from airflow.operators.python import PythonOperator
 
 DEFAULT_DATE = datetime(2016, 1, 1)
 
@@ -8,5 +8,13 @@ default_args = {
     "owner": "airflow",
     "start_date": DEFAULT_DATE,
 }
+
+def hello_world():
+    print("hellow World")
+
+    
 with DAG(dag_id="test_dag_with_no_tags", default_args=default_args, schedule_interval='@once') as dag:
-    task_a = DummyOperator(task_id="test_task_a")
+    task_a = PythonOperator(
+        task_id="test_task_a",
+        python_callable=hello_world
+    )
