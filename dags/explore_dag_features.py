acf49c65b903f062911c0bb2477d3b118761589a
shivamg-git <gupta.shivam.work@gmail.com>
2024-02-21 01:35:59 +0530

    DAG

--- dags/explore_dag_features.py
+++ dags/explore_dag_features.py
@@ -126,6 +126,6 @@ with DAG(
 
     bash_operator4 = BashOperator(
         task_id = "bash4",
-        bash_command="{{ params.x }},{{ params.y}}",
+        bash_command="echo '{{ params.x }},{{ params.y}}'",
         dag=dag
     )
