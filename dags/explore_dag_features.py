d6baa0d92d2be1adc0b4e2f1293ac138f3eef15d
shivamg-git <gupta.shivam.work@gmail.com>
2024-02-21 00:02:45 +0530

    DAG

--- dags/explore_dag_features.py
+++ dags/explore_dag_features.py
@@ -15,7 +15,7 @@ default_args= {
     'email_on_failure': True,
     'email_on_retry': True,
     'retries': 1,
-    'retry_delay': timedelta(minutes=5),
+    'retry_delay': timedelta(minutes=5)
     # 'queue': 'bash_queue',
     # 'pool': 'backfill',
     # 'priority_weight': 10,
