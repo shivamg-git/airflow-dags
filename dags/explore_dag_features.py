61e052d67bbf78d602561db4f863add2e71b79cc
shivamg-git <gupta.shivam.work@gmail.com>
2024-02-21 00:00:55 +0530

    DAG

--- dags/explore_dag_features.py
+++ dags/explore_dag_features.py
@@ -1,6 +1,35 @@
 from datetime import datetime
 from airflow import DAG
 from airflow.operators.bash import BashOperator
+from airflow.utils.dates import days_ago
+
+# A dictionary of default parameters to be used by all tasks in the DAG.
+# https://airflow.apache.org/docs/apache-airflow/1.10.9/tutorial.html
+# These args will get passed on to each operator
+# You can override them on a per-task basis during operator initialization
+default_args= {
+    "owner" : "spider",                 # Owner of all tasks
+    'depends_on_past': False,           # depends_on_past (boolean) when set to True, keeps a task from getting triggered if the previous schedule for the task hasn’t succeeded.
+    'start_date': days_ago(2),
+    'email': ['gupta.shivamg.work@gmail.com'],
+    'email_on_failure': True,
+    'email_on_retry': True,
+    'retries': 1,
+    'retry_delay': datetime.timedelta(minutes=5),
+    # 'queue': 'bash_queue',
+    # 'pool': 'backfill',
+    # 'priority_weight': 10,
+    # 'end_date': datetime(2016, 1, 1),
+    # 'wait_for_downstream': False,
+    # 'dag': dag,
+    # 'sla': timedelta(hours=2),
+    # 'execution_timeout': timedelta(seconds=300),
+    # 'on_failure_callback': some_function,
+    # 'on_success_callback': some_other_function,
+    # 'on_retry_callback': another_function,
+    # 'sla_miss_callback': yet_another_function,
+    # 'trigger_rule': 'all_success'
+}
 
 
 with DAG(
@@ -30,8 +59,14 @@ with DAG(
 
     # a dictionary of filters that will be exposed in your jinja templates.
     user_defined_filters = {
-        "hello":lambda name: 'Hello %s' % name
-    }
+        "hello":lambda name: 'Hello %s' % name,
+        "Hi":lambda name: 'Hi %s' % name,
+    },
+
+    #  A dictionary of default parameters to be used as constructor keyword parameters when initialising operators
+    default_args =  default_args
+    
+
 
 
 
