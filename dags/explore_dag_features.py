e8ca38c266ac711c240665c9890d091cfe1d7fb3
shivamg-git <gupta.shivam.work@gmail.com>
2024-02-21 00:39:21 +0530

    DAG

--- dags/explore_dag_features.py
+++ dags/explore_dag_features.py
@@ -3,6 +3,12 @@ from airflow import DAG
 from airflow.operators.bash import BashOperator
 from airflow.utils.dates import days_ago
 
+def custom_success_callback(context):
+    print("Task has completed successfully!")
+    print(repr(context))
+    return True
+
+
 # A dictionary of default parameters to be used by all tasks in the DAG.
 # https://airflow.apache.org/docs/apache-airflow/1.10.9/tutorial.html
 # These args will get passed on to each operator
@@ -16,21 +22,24 @@ default_args= {
     'email_on_retry': False,
     'retries': 1,
     'retry_delay': timedelta(minutes=2),
+    'execution_timeout': timedelta(seconds=10),
+    # https://marclamberti.com/blog/airflow-trigger-rules-all-you-need-to-know/
+    # all_success, all_failed, all_done, one_failed, one_success, none_failed, none_skipped, none_failed_min_one_success
+    'trigger_rule': 'all_success',
+
 
     # 'queue': 'bash_queue',
     # 'pool': 'backfill',
     # 'priority_weight': 10,
     # 'wait_for_downstream': False,
     # 'sla': timedelta(hours=2),
-    'execution_timeout': timedelta(seconds=10),
+
     # 'on_failure_callback': some_function,
-    # 'on_success_callback': some_other_function,
+    'on_success_callback': custom_success_callback
     # 'on_retry_callback': another_function,
     # 'sla_miss_callback': yet_another_function,
     
-    # https://marclamberti.com/blog/airflow-trigger-rules-all-you-need-to-know/
-    # all_success, all_failed, all_done, one_failed, one_success, none_failed, none_skipped, none_failed_min_one_success
-    'trigger_rule': 'all_success'
+
 }
 
 
