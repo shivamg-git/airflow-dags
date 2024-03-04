6f0aa2d3c90fcf6e989ead8cc5c2a54d1dd2fe5a
shivamg-git <gupta.shivam.work@gmail.com>
2024-03-04 17:53:38 +0530

    test

--- dags/spark_dag.py
+++ dags/spark_dag.py
@@ -2,13 +2,14 @@ from airflow import DAG
 from airflow.decorators import task
 from airflow.operators.empty import EmptyOperator
 from datetime import datetime, timedelta 
+from airflow.providers.cncf.kubernetes.operators.pod import KubernetesPodOperator
 
 dag_owner = 'Spider'
 
 default_args = {'owner': dag_owner,
         'depends_on_past': False,
         'retries': 100,
-        'retry_delay': timedelta(minutes=5)
+        'retry_delay': timedelta(seconds=20)
         }
 
 dag = DAG(dag_id='test_k8_retries',
@@ -24,9 +25,16 @@ start = EmptyOperator(task_id='start', dag=dag)
 
 parallel_task = []
 for i in range(100):
-    taskX = EmptyOperator(
+    taskX = KubernetesPodOperator(
         task_id=f"{i}_operator",
-        dag=dag
+        dag=dag,
+        kubernetes_conn_id="local_k8_connection",
+        namespace="airflow",
+        image="docker.io/library/python:latest",
+        cmds=["sh","-c"],
+        arguments=["sleep 60"],
+        get_logs=True,
+        deferrable=True
     )
     parallel_task.append(taskX)
 
